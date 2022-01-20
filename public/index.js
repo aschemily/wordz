384fe0cceb46eece65c90e9686cafeea382fbaee
aschemily <aschemily@gmail.com>
2022-01-20 18:48:14 -0500

    functionality

--- public/index.js
+++ public/index.js
@@ -6,32 +6,68 @@ const wordList = document.getElementById('word-list')
 
 let example = "tests"
 
-function wordOfTheDay(word, userWord){
-   // console.log('word?', word, userWord)
-   let userList = document.createElement('li')
-   // userList.setAttribute("id", "wordId")
-   
-    let splitWord = word.split('')
-    let userInput = userWord.split('')
-
-
-for(let i = 0; i < userInput.length; i++){
-   // console.log(userInput[i], i, 'word', splitWord[0])
-    if(userInput[i] === splitWord[0] ){    
-       userList.innerHTML += `<span style='color: green;'>${userInput[i]}</span>`
-    }else if(word.includes(userInput[i])){
-        userList.innerHTML += `<span style='color: yellow;'>${userInput[i]}</span>`
-    }else{
-        userList.innerHTML += `<span style='color: black;'>${userInput[i]}</span>`
-    }
-
-
-}  
-
-    wordList.appendChild(userList)
+// function wordOfTheDay(word, userWord){
   
-}
+//    let userList = document.createElement('li')
+//     let splitWord = word.split('')
+//     let userInput = userWord.split('')
+  
+//     for(let i = 0; i < splitWord.length; i++){
+//         for(let j = 0; j < userInput.length; j++){
 
+//             //console.log('word', splitWord[i], i, "user input", userInput[j], j)
+
+//            if(splitWord[i] === userInput[j] && i === j ){
+//               console.log('user word', userInput[j], 'actual word', splitWord[i])
+              
+//              // letter.innerHTML = `<span style='color: green;'>${userInput[j]}</span>`
+              
+//            } else if(word.includes(userInput[j])){
+//                 console.log('inside second  if', userInput[j], 'word is', word, 'j', j, 'i', i)
+//              //  userList.innerHTML = `<span style='color: orange;'>${userInput[j]}</span>`
+              
+               
+//            }
+           
+//         }
+     
+    
+//     }
+//     wordList.appendChild(userList)
+  
+// }
+
+function wordOfTheDay(word, userWord){
+   // console.log('word', word, 'user word', userWord)
+    let userList = document.createElement('li')
+    let splitUserWord = userWord.split('')
+    let splitWord = word.split('')
+
+    // if(word === userWord){
+    //     userList.innerHTML = `<span style='color: green;'>${userWord}</span>`
+    // }
+
+    splitUserWord.forEach((ltr, i)=>{
+       // console.log(ltr, i)
+       // console.log(i, ltr, splitWord[0])
+       // console.log(i, ltr, splitWord[1])
+      if((i === 0 && ltr === splitWord[0]) || (i === 1 && ltr === splitWord[1]) || (i === 2 && ltr === splitWord[2]) || (i === 3 && ltr === splitWord[3]) || (i === 4 && ltr === splitWord[4])){
+          console.log('ltr', ltr, i)
+          userList.innerHTML += `<span style='color: green;'>${ltr}</span>`
+      }else if(word.includes(ltr)){
+          console.log('letters in word', ltr)
+          userList.innerHTML += `<span style='color: orange;'>${ltr}</span>`
+      }else{
+          console.log('no match', ltr)
+          userList.innerHTML += `<span style='color: black;'>${ltr}</span>`
+      }
+      
+
+      
+    })
+ 
+    wordList.appendChild(userList)
+}
 
 
 function userSubmitted(e){
